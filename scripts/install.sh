f5b084c94eac09f842a7a864a575a0cd73e7712a
Zakariaxor <57765769+Zakariaxor@users.noreply.github.com>
2020-11-11 02:35:29 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -30,15 +30,13 @@ gen_3proxy() {
 daemon
 maxconn 1000
 nscache 65536
-timeouts 1 5 30 60 180 1800 15 60
+timeouts 1 5 30 200 180 2500 15 60
 setgid 65535
 setuid 65535
 flush
 auth none
 
 $(awk -F "/" '{
-print "auth none\n" \
-"allow * \n" \
 print "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
